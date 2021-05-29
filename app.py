62887b6b016d48237ae04874ca96ab4f10445897
Kaustubh Singh <kaustubh.singh@gmail.com>
2021-05-29 15:23:48 +0000

    additions or edits

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ def index():
      ["24/01/2021", 65542], ["03/02/2021", 75427], ["10/02/2021", 51315]]
 
     chart_data = [
-        { "week" : "10/01/2021", "outcome" : 20347 },
-        { "week" : "17/01/2021", "outcome" : 32847 },
-        { "week" : "24/01/2021", "outcome" : 65542 },
-        { "week" : "03/02/2021", "outcome" : 75427 },
-        { "week" : "10/02/2021", "outcome" : 51315 }
+        { "week" : "10-01-2021", "outcome" : 20347 },
+        { "week" : "17-01-2021", "outcome" : 32847 },
+        { "week" : "24-01-2021", "outcome" : 65542 },
+        { "week" : "03-02-2021", "outcome" : 75427 },
+        { "week" : "10-02-2021", "outcome" : 51315 }
     ]
 
     return render_template('index.html', data=data, chart_data=chart_data)
